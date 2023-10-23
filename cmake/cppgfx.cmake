12fd92391cdd467f6cd9d849a7ec4c861501a1ef
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-10-23 11:51:21 +0200

    Implement batch scripting

--- cmake/cppgfx.cmake
+++ cmake/cppgfx.cmake
@@ -1,6 +1,7 @@
 
 include(FetchContent)
 
+message(STATUS "Fetching cppgfx")
 FetchContent_Declare(cppgfx
     GIT_REPOSITORY https://github.com/HerrNamenlos123/cppgfx.git
     GIT_TAG        main
---
+++ run.bat
@@ -0,0 +1,42 @@
+@echo off
+
+:: Batch
+
+:: If the file does not exist
+if not exist "build/cppgfx.configured" (
+    echo [34m[cppgfx] The project was not configured yet, configuring now...[0m
+
+    :: Create build directory
+    mkdir build 2> nul
+
+    :: Configure the project
+    cmake -S . -B build
+
+    if %errorlevel% neq 0 (
+        echo [cppgfx] Configuring failed
+        exit /b 1
+    )
+
+    :: Create the file
+    echo. > "build/cppgfx.configured"
+
+    echo [cppgfx] Configuring done
+)
+
+echo [34m[cppgfx] Building the project...[0m
+
+:: Build the project
+cmake --build build
+
+if %errorlevel% neq 0 (
+    echo [31m[cppgfx] Building failed[0m
+    exit /b 1
+)
+
+echo [34m[cppgfx] Building done[0m
+
+:: Run the project
+
+echo [34m[cppgfx] Running the project...[0m
+
+call build/run
