43d7b8062706770585b035232a0bac73022896c8
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-10-23 11:08:10 +0200

    Add basic project layout

---
+++ cmake/cppgfx.cmake
@@ -0,0 +1,8 @@
+
+include(FetchContent)
+
+FetchContent_Declare(cppgfx
+    GIT_REPOSITORY https://github.com/HerrNamenlos123/cppgfx.git
+    GIT_TAG        main
+)
+FetchContent_MakeAvailable(cppgfx)
